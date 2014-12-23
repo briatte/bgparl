6629123afb9c2a8685d8b794d47c23f440cc6b46
François <briatte@gmail.com>
2014-12-23 13:28:16 +0100

    ParlGov scores update

--- parties.r
+++ parties.r
@@ -2,9 +2,9 @@
 
 colors = c(
   "KB" = "#E41A1C",   # Коалиция за България (Coalition for Bulgaria, led by Socialists) -- red
-  "NMS" = "#FFFF33",  # Национално движение Симеон Втори (National Movement Simeon the Second) -- yellow
   "DPS" = "#80B1D3",  # Движение за права и свободи (Movement for Rights and Freedoms) -- light blue
   "A" = "#A65628",    # Атака (Ataka) -- brown
+  "NMS" = "#FFFF33",  # Национално движение Симеон Втори (National Movement Simeon the Second) -- yellow
   "BNS" = "#FF7F00",  # Български Народен Съюз (Bulgarian People's Union) -- orange
   "ODS" = "#BEBADA",  # Обединени Демократични Сили (United Democratic Forces, 2005 election) -- light purple
   "GERB" = "#984EA3", # ГЕРБ (Citizens for European Development of Bulgaria) -- purple
@@ -16,10 +16,10 @@ colors = c(
 # ParlGov Left/Right scores
 
 scores = c(
-  "KB" = 2.8,
-  "NMS" = 4.6,
+  "KB" = 2.9,
   "DPS" = 4.6,
   "A" = 5.5,
+  "NMS" = 5.8,
   "BNS" = 5.8,
   "ODS" = 7,
   "GERB" = 7.4,
